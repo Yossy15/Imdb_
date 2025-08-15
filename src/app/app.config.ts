7fbf8dd6e56ba30543d84c662a22a4b799285dca
Gifsho <padyod6@gmail.com>
2025-08-15 19:43:42 +0700

    up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync(), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ]
 };
